e202b21093e78996751fdffd98800fc0f450f2a5
Emmanuel434 <eahenkan@aimsammi.org>
2020-06-08 01:23:14 +0000

    removed

--- dev/js/body.js
+++
@@ -1,9 +0,0 @@
-// Include body scripts in the right order
-// They will be concatenated using gulp-rigger in body.min.js
-
-//= vendor/jquery.js
-//= lib/jquery.navigation.js
-//= lib/settings.js
-
-// Google map disabled by default because of empty API key
-// It is still available at lib/gmap.js
--- dev/js/head.js
+++
@@ -1,4 +0,0 @@
-// Include head scripts in the right order
-// They will be concatenated using gulp-rigger in head.min.js
-
-//= lib/progress.js
